e1bd50c6de5095fc4cc1b2747c6c7a7a877d2271
Dell-User <Dell-User@DESKTOP-B16SGH2>
2023-09-06 01:01:20 +0200

    Add project files.

---
+++ 5 Sept 2023/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtGender = New System.Windows.Forms.TextBox()
+        Me.txtSurname = New System.Windows.Forms.TextBox()
+        Me.btnGreet = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblSurname = New System.Windows.Forms.Label()
+        Me.lblGender = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(181, 74)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 26)
+        Me.txtName.TabIndex = 0
+        '
+        'txtGender
+        '
+        Me.txtGender.Location = New System.Drawing.Point(181, 186)
+        Me.txtGender.Name = "txtGender"
+        Me.txtGender.Size = New System.Drawing.Size(100, 26)
+        Me.txtGender.TabIndex = 1
+        '
+        'txtSurname
+        '
+        Me.txtSurname.Location = New System.Drawing.Point(181, 133)
+        Me.txtSurname.Name = "txtSurname"
+        Me.txtSurname.Size = New System.Drawing.Size(100, 26)
+        Me.txtSurname.TabIndex = 2
+        '
+        'btnGreet
+        '
+        Me.btnGreet.Location = New System.Drawing.Point(147, 245)
+        Me.btnGreet.Name = "btnGreet"
+        Me.btnGreet.Size = New System.Drawing.Size(100, 85)
+        Me.btnGreet.TabIndex = 3
+        Me.btnGreet.Text = "Greet me!"
+        Me.btnGreet.UseVisualStyleBackColor = True
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(88, 80)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(51, 20)
+        Me.lblName.TabIndex = 4
+        Me.lblName.Text = "Name"
+        '
+        'lblSurname
+        '
+        Me.lblSurname.AutoSize = True
+        Me.lblSurname.Location = New System.Drawing.Point(88, 139)
+        Me.lblSurname.Name = "lblSurname"
+        Me.lblSurname.Size = New System.Drawing.Size(74, 20)
+        Me.lblSurname.TabIndex = 5
+        Me.lblSurname.Text = "Surname"
+        '
+        'lblGender
+        '
+        Me.lblGender.AutoSize = True
+        Me.lblGender.Location = New System.Drawing.Point(88, 192)
+        Me.lblGender.Name = "lblGender"
+        Me.lblGender.Size = New System.Drawing.Size(63, 20)
+        Me.lblGender.TabIndex = 6
+        Me.lblGender.Text = "Gender"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblGender)
+        Me.Controls.Add(Me.lblSurname)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.btnGreet)
+        Me.Controls.Add(Me.txtSurname)
+        Me.Controls.Add(Me.txtGender)
+        Me.Controls.Add(Me.txtName)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtGender As TextBox
+    Friend WithEvents txtSurname As TextBox
+    Friend WithEvents btnGreet As Button
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblSurname As Label
+    Friend WithEvents lblGender As Label
+End Class
---
+++ 5 Sept 2023/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub TxtName_TextChanged(sender As Object, e As EventArgs) Handles txtName.TextChanged
+
+    End Sub
+
+    Private Sub BtnGreet_Click(sender As Object, e As EventArgs) Handles btnGreet.Click
+        Dim strName As String
+        Dim strSurname As String
+        Dim strGender As String
+
+        strName = txtName.Text
+        strSurname = txtSurname.Text
+        strGender = txtGender.Text
+        MessageBox.Show("Hi" & " " & strName & " " & strSurname)
+    End Sub
+End Class
